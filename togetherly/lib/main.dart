eaf9e64c1f82e4e00ca71bda86a8505bc6ad81ba
kyliecallaway404 <90010624+kyliecallaway404@users.noreply.github.com>
2024-02-22 21:39:26 -0700

    Update main.dart

--- togetherly/lib/main.dart
+++ togetherly/lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: 'https://ejsdquitvcfduhsrdwvx.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqc2RxdWl0dmNmZHVoc3Jkd3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NDM1NjgsImV4cCI6MjAyMjQxOTU2OH0._AkLYJzKGz8LH_sX6r1mvm0ydAl3l09qGQZ_N-6i4rQ',
+    anonKey: '',
   );
   runApp(const MyApp());
 }
