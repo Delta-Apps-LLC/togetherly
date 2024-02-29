6ec70b7acf7f156b7917198cb5b6e0aac544c2b5
Ryan Harper <harper61.r@gmail.com>
2024-02-29 13:23:32 -0700

    fix choreService, add person and family service

--- togetherly/lib/services/personService.dart
+++ togetherly/lib/services/personService.dart
@@ -1,42 +1,40 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:togetherly/models/chore.dart';
+import '../models/person.dart';
 
-
-class ChoreService {
-
-  Future<List<Chore>> getChores() async {
-    var result = await Supabase.instance.client.from('Chore')
-        .select('id, assignedPerson,title, description, dueDate, points, status');
-    // result might give a list of database rows
-    List<Chore> chores = [];
-    result.forEach((map) {
-      var chore = Chore(
-          map['id'], map['personId'], map['title'], map['description'],map['dueDate'], map['points'], map['status'], map[shared]);
-      chores.add(chore);
-    });
-
-    return Future<List<Chore>>.value(chores);
-  }
-
-  Future<List<Chore>> getChoreList(int personId) async{
-    //Sort List Today, upcoming and overdue
+class PersonService {
+  Future<List<Person>> getFamily(int personId) async {
     var result = await Supabase.instance.client.from('Chore')
         .select('id, assignedPerson,title, description, dueDate, points, status')
-        .eq('personId', personId);
-    List<Chore> chores = [];
+        .match({ 'id': personId });
+    // result might give a list of database rows
+    List<Person> family = [];
     result.forEach((map) {
-      var chore = Chore(
+      var person = Person(
           map['id'], map['personId'], map['title'], map['description'],map['dueDate'], map['points'], map['status'], map[shared]);
-      chores.add(chore);
+      family.add(chore);
     });
 
-    return Future<List<Chore>>.value(chores);
+    return Future<List<Person>>.value(family);
   }
 
-  Future<void> insertChore(Chore chore) async {
+  Future<Person> getPerson(Person person) async {
+    var result = await Supabase.instance.client.from('Person')
+        .select('id, assignedPerson,title, description, dueDate, points, status')
+        .match({ 'id': person.id });
+    // result might give a list of database rows
+    var person;
+    result.forEach((map) {
+      person = Person(
+          map['id'], map['personId'], map['title'], map['description'],map['dueDate'], map['points'], map['status'], map[shared]);
+    });
+
+    return Future<Person>.value(person);
+  }
+
+  Future<void> insertPerson(Person person) async {
     //Service function call and pass chore
     await Supabase.instance.client
-        .from('Chore')
+        .from('Person')
         .insert({
       'title': chore.title,
       'description': chore.description,
@@ -47,18 +45,18 @@ class ChoreService {
     });
   }
 
-  Future<void> deleteChore(Chore chore) async {
+  Future<void> deletePerson(Person person) async {
     //await
     await Supabase.instance.client
-        .from('Chore')
+        .from('Person')
         .delete()
-        .match({ 'id': chore.id });
+        .match({ 'id': person.id });
   }
 
-  Future<void> updateChore(Chore chore) async {
+  Future<void> updatePerson(Person person) async {
     //Query by choreID
     await Supabase.instance.client
-        .from('Chore')
+        .from('Person')
         .update({
       'title': chore.title,
       'description': chore.description,
