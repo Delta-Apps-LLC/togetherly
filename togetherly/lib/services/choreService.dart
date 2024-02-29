88d2abd7e8864878d94d98dacd246cd43685d64a
Ryan Harper <harper61.r@gmail.com>
2024-02-28 20:28:46 -0700

    Begin implementation of choreService

--- togetherly/lib/services/choreService.dart
+++ togetherly/lib/services/choreService.dart
@@ -10,30 +10,66 @@ class ChoreService {
         .select('id, assignedPerson,title, description, dueDate, points, status');
     // result might give a list of database rows
     List<Chore> chores = [];
-    // result.forEach((map) {
-    //   var chore = Chore(
-    //   map['id'], map['assignedPerson'], map['title'], map['description'],map['dueDate'], map['points'], map['status']);
-    //   chores.add(chore);
-    // });
+    result.forEach((map) {
+      var chore = Chore(
+          map['id'], map['personId'], map['title'], map['description'],map['dueDate'], map['points'], map['status'], map[shared]);
+      chores.add(chore);
+    });
 
     return Future<List<Chore>>.value(chores);
   }
 
   Future<List<Chore>> getChoreList(int personId) async{
     //Sort List Today, upcoming and overdue
-    return [];
+    var result = await Supabase.instance.client.from('Chore')
+        .select('id, assignedPerson,title, description, dueDate, points, status')
+        .eq('personId', personId);
+    // result might give a list of database rows
+    List<Chore> chores = [];
+    result.forEach((map) {
+      var chore = Chore(
+          map['id'], map['personId'], map['title'], map['description'],map['dueDate'], map['points'], map['status'], map[shared]);
+      chores.add(chore);
+    });
+
+    return Future<List<Chore>>.value(chores);
   }
 
   Future<void> addChore(Chore chore) async {
     //Service function call and pass chore
+    await supabase
+        .from('cities')
+        .insert({
+      'title': chore.title,
+      'description': chore.description,
+      'title': chore.title,
+      'description': chore.description,
+      'title': chore.title,
+      'description': chore.description
+    });
   }
 
   Future<void> deleteChore(Chore chore) async {
     //await
+    await supabase
+        .from('cities')
+        .delete()
+        .match({ 'id': chore.id });
   }
 
   Future<void> updateChore(Chore newChore) async {
     //Query by choreID
+    await supabase
+        .from('cities')
+        .update({
+      'title': chore.title,
+      'description': chore.description,
+      'title': chore.title,
+      'description': chore.description,
+      'title': chore.title,
+      'description': chore.description
+    })
+        .match({ 'id': chore.id });
   }
 
 }
