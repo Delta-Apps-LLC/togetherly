aeac1e822d74d5b31ecbfe845245b7d148c9ce37
kyliecallaway404 <90010624+kyliecallaway404@users.noreply.github.com>
2024-03-01 15:23:39 -0700

    updated the chore constructor based on meeting feedback

--- togetherly/lib/models/chore.dart
+++ togetherly/lib/models/chore.dart
@@ -11,32 +11,27 @@ enum ChoreStatus {
 
 class Chore {
 
-  final int _id;
-  final Child _assignedChild;
-  final String _title;
-  final String _description;
-  final DateTime _dueDate;
-  final int _points;
-  final ChoreStatus _status;
-  final bool _isShared;
+  final int? id;
+  final int assignedChildId;
+  final String title;
+  final String description;
+  final DateTime dueDate;
+  final int points;
+  final ChoreStatus status;
+  final bool isShared;
 
-  Chore(this._id, this._assignedChild, this._title, this._description,
-      this._dueDate, this._points, this._status, this._isShared);
+  const Chore({this.id, required this.assignedChildId, required this.title, this.description = '',
+      required this.dueDate, required this.points, this.status = ChoreStatus.assigned, required this.isShared});
 
-  ChoreStatus get status => _status;
-
-  int get points => _points;
-
-  DateTime get dueDate => _dueDate;
-
-  String get description => _description;
-
-  String get title => _title;
-
-  Child get assignedPerson => _assignedChild;
-
-  int get id => _id;
-
-  bool get isshared => _isShared;
+  // Chore(Chore original, {int? assignedChildId, String? title, String? description,
+  //   DateTime? dueDate, int? points, ChoreStatus? status, bool? isShared})
+  // : id = original.id,
+  //   assignedChildId = assignedChildId ?? original.assignedChildId,
+  //   title = title ?? original.title,
+  //   description = description ?? original.description,
+  //   dueDate = dueDate ?? original.dueDate,
+  //   points = points ?? original.points,
+  //   status = status ?? original.status,
+  //   isShared = isShared ?? original.isShared;
 
 }
