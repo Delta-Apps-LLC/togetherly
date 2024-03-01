c104568cbb10fabdb191e0206b1d503420c13eca
kyliecallaway404 <90010624+kyliecallaway404@users.noreply.github.com>
2024-02-29 18:27:10 -0700

    Update Famiily Model

--- togetherly/lib/models/family.dart
+++ togetherly/lib/models/family.dart
@@ -6,8 +6,9 @@ class Family {
 
   final int _id;
   String _name;
-  List<Parent> _parents;
-  List<Child> _children;
+  //List<Parent> _parents;
+  //List<Child> _children;
 
-  Family(this._id, this._name, this._parents, this._children);
+  Family(this._id, this._name);
+
 }
