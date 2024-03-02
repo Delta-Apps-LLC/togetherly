e12725b18e76a6708e18808f569fabe3a398cade
Nathan Walker <nathanwalker1208@gmail.com>
2024-03-01 17:31:39 -0700

    Clean up ChoreService

--- togetherly/lib/services/chore_service.dart
+++ togetherly/lib/services/chore_service.dart
@@ -5,23 +5,7 @@ class ChoreService {
   Future<List<Chore>> getChores() async {
     var result = await Supabase.instance.client.from('Chore').select(
         'id, assignedPerson,title, description, dueDate, points, status');
-    // result might give a list of database rows
-    List<Chore> chores = [];
-    result.forEach((map) {
-      var chore = Chore(
-        id: map['id'],
-        assignedChildId: map['personId'],
-        title: map['title'],
-        description: map['description'],
-        dueDate: map['dueDate'],
-        points: map['points'],
-        status: map['status'],
-        isShared: map['shared'],
-      );
-      chores.add(chore);
-    });
-
-    return Future<List<Chore>>.value(chores);
+    return result.map(_mapToChore).toList();
   }
 
   Future<List<Chore>> getChoreList(int personId) async {
@@ -31,34 +15,12 @@ class ChoreService {
         .select(
             'id, assignedPerson,title, description, dueDate, points, status')
         .eq('personId', personId);
-    List<Chore> chores = [];
-    result.forEach((map) {
-      var chore = Chore(
-        id: map['id'],
-        assignedChildId: map['personId'],
-        title: map['title'],
-        description: map['description'],
-        dueDate: map['dueDate'],
-        points: map['points'],
-        status: map['status'],
-        isShared: map['shared'],
-      );
-      chores.add(chore);
-    });
-
-    return Future<List<Chore>>.value(chores);
+    return result.map(_mapToChore).toList();
   }
 
   Future<void> insertChore(Chore chore) async {
     //Service function call and pass chore
-    await Supabase.instance.client.from('Chore').insert({
-      'title': chore.title,
-      'description': chore.description,
-      'dateDue': chore.dueDate,
-      'points': chore.points,
-      'status': chore.status,
-      'personId': chore.assignedChildId,
-    });
+    await Supabase.instance.client.from('Chore').insert(_choreToMap(chore));
   }
 
   Future<void> deleteChore(Chore chore) async {
@@ -71,13 +33,29 @@ class ChoreService {
 
   Future<void> updateChore(Chore chore) async {
     //Query by choreID
-    await Supabase.instance.client.from('Chore').update({
-      'title': chore.title,
-      'description': chore.description,
-      'dateDue': chore.dueDate,
-      'points': chore.points,
-      'status': chore.status,
-      'personId': chore.assignedChildId,
-    }).match({'id': chore.id});
+    await Supabase.instance.client
+        .from('Chore')
+        .update(_choreToMap(chore))
+        .match({'id': chore.id});
   }
+
+  Chore _mapToChore(Map<String, dynamic> map) => Chore(
+        id: map['id'],
+        assignedChildId: map['personId'],
+        title: map['title'],
+        description: map['description'],
+        dueDate: map['dueDate'],
+        points: map['points'],
+        status: map['status'],
+        isShared: map['shared'],
+      );
+
+  Map<String, dynamic> _choreToMap(Chore chore) => {
+        'title': chore.title,
+        'description': chore.description,
+        'dateDue': chore.dueDate,
+        'points': chore.points,
+        'status': chore.status,
+        'personId': chore.assignedChildId,
+      };
 }
