a73133eabfe248251b0de765dd9ba09820912304
Nathan Walker <nathanwalker1208@gmail.com>
2024-03-19 15:07:46 -0600

    Create RewardService stubs

---
+++ togetherly/lib/services/reward_service.dart
@@ -0,0 +1,20 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../models/reward.dart';
+
+class RewardService {
+  Future<List<Reward>> getRewards(int familyId) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> insertReward(Reward reward) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> deleteReward(Reward reward) async {
+    throw UnimplementedError();
+  }
+
+  Future<void> updateReward(Reward reward) async {
+    throw UnimplementedError();
+  }
+}
