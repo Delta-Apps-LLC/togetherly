36f75fae7d8a5524d981f5c9a11a39e157f439ad
Nathan Walker <nathanwalker1208@gmail.com>
2024-03-19 15:10:44 -0600

    Create basic RewardProvider

---
+++ togetherly/lib/providers/reward_provider.dart
@@ -0,0 +1,51 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:togetherly/models/reward.dart';
+import 'package:togetherly/providers/user_identity_provider.dart';
+import 'package:togetherly/services/reward_service.dart';
+
+class RewardProvider with ChangeNotifier {
+  RewardProvider(this._service, this._userIdentityProvider) {
+    log("RewardProvider created");
+    refresh();
+  }
+
+  final RewardService _service;
+
+  UserIdentityProvider _userIdentityProvider;
+
+  List<Reward> _rewards = [];
+  List<Reward> get rewards => _rewards;
+
+  Future<void> addReward(Reward reward) async {
+    await _service.insertReward(reward);
+    await refresh();
+  }
+
+  Future<void> deleteReward(Reward reward) async {
+    await _service.deleteReward(reward);
+    await refresh();
+  }
+
+  Future<void> updateReward(Reward reward) async {
+    await _service.updateReward(reward);
+    await refresh();
+  }
+
+  Future<void> refresh() async {
+    final familyId = _userIdentityProvider.familyId;
+    if (familyId != null) {
+      _rewards = await _service.getRewards(familyId);
+    } else {
+      _rewards = [];
+    }
+    notifyListeners();
+    log("RewardProvider refreshed!");
+  }
+
+  void updateDependencies(UserIdentityProvider userIdentityProvider) {
+    _userIdentityProvider = userIdentityProvider;
+    refresh();
+  }
+}
