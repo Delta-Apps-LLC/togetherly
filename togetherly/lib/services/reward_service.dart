8aa5e2965808fa5d27f4b5ccb47cdfabf4b1929f
Ryan Harper <harper61.r@gmail.com>
2024-03-21 12:57:03 -0600

    implement reward service

---
+++ togetherly/lib/services/reward_service.dart
@@ -0,0 +1,53 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:togetherly/models/reward.dart';
+
+class RewardService {
+  static const String _rewardTable = "reward";
+
+  Future<List<Reward>> getChoresByFamily(int familyId) async {
+    var result = await Supabase.instance.client
+        .from(_rewardTable)
+        .select("id, title, description, points, quantity, family_id")
+        .eq("family_id", familyId);
+    return result.map(_mapToReward).toList();
+  }
+
+  Future<void> insertChore(Reward reward) async {
+    //Service function call and pass chore
+    await Supabase.instance.client.from(_rewardTable)
+        .insert(_rewardToMap(reward));
+  }
+
+  Future<void> deleteChore(Reward reward) async {
+    //await
+    await Supabase.instance.client
+        .from(_rewardTable)
+        .delete()
+        .match({'id': reward.id});
+  }
+
+  Future<void> updateChore(Reward reward) async {
+    //Query by choreID
+    await Supabase.instance.client
+        .from(_rewardTable)
+        .update(_rewardToMap(reward))
+        .match({'id': reward.id});
+  }
+
+  Reward _mapToReward(Map<String, dynamic> map) => Reward(
+    id: map['id'],
+    family_id: map['family_id'],
+    title: map['title'],
+    description: map['description'],
+    points: map['points'],
+    quantity: map['quantity'],
+  );
+
+  Map<String, dynamic> _rewardToMap(Reward reward) => {
+    'title': reward.title,
+    'description': reward.description,
+    'date_due': reward.points,
+    'points': reward.quantity,
+    'shared': reward.family_id,
+  };
+}
