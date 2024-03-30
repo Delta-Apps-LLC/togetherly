0c909869dee7aed14b9e016b572549af5ea31b8c
kyliecallaway404 <90010624+kyliecallaway404@users.noreply.github.com>
2024-03-30 17:14:07 -0600

    Made variable private and typeset based on PR request feedback

--- togetherly/lib/providers/chore_provider.dart
+++ togetherly/lib/providers/chore_provider.dart
@@ -7,14 +7,16 @@ import 'package:togetherly/providers/user_identity_provider.dart';
 import 'package:togetherly/services/assignment_service.dart';
 import 'package:togetherly/services/chore_service.dart';
 
+import '../models/child.dart';
+
 class ChoreProvider with ChangeNotifier {
-  ChoreProvider(this.choreService, this._userIdentityProvider) {
+  ChoreProvider(this._choreService, this._assignmentService, this._userIdentityProvider) {
     log("ChoreProvider created");
     refresh();
   }
 
-  final ChoreService choreService;
-  final AssignmentService assignmentService;
+  final ChoreService _choreService;
+  final AssignmentService _assignmentService;
 
   UserIdentityProvider _userIdentityProvider;
 
@@ -29,26 +31,30 @@ class ChoreProvider with ChangeNotifier {
   //   return personId == null ? null : choresAssignedToPerson(personId);
   // }
 
-  Future<void> addChore(Chore chore, children) async {
-    chore = await choreService.insertChore(chore);
+ /* List<Chore> getChoreByChild(int childId) {
+
+  }*/
+  Future<void> addChore(Chore chore, List<Child> children) async {
+    //Will update to capture the chore when chore service is updated.
+    await _choreService.insertChore(chore);
     for (final c in children) {
-      Assignment assignment = Assignment(personId: c, choreId: chore.id!);
-      await assignmentService.insertAssignment(assignment);
+      Assignment assignment = Assignment(personId: c.id!, choreId: chore.id!);
+      await _assignmentService.insertAssignment(assignment);
     }
     await refresh();
   }
 
-  Future<void> deleteChore(Chore chore, children) async {
-    await choreService.deleteChore(chore);
-    for (final c in children) {
-      Assignment assignment = Assignment(personId: c, choreId: chore.id!);
-      await assignmentService.deleteAssignment(assignment);
-    }
+  Future<void> deleteChore(Chore chore) async {
+    await _choreService.deleteChore(chore);
+    // for (final c in children) {
+    //   Assignment assignment = Assignment(personId: c.id, choreId: chore.id!);
+    //   await _assignmentService.deleteAssignment(assignment);
+    // }
     await refresh();
   }
 
   Future<void> updateChore(Chore chore) async {
-    await choreService.updateChore(chore);
+    await _choreService.updateChore(chore);
     await refresh();
   }
 
@@ -61,7 +67,7 @@ class ChoreProvider with ChangeNotifier {
   Future<void> refresh() async {
     final familyId = _userIdentityProvider.familyId;
     if (familyId != null) {
-      _allChores = await choreService.getChoresByFamily(familyId);
+      _allChores = await _choreService.getChoresByFamily(familyId);
     } else {
       _allChores = [];
     }
