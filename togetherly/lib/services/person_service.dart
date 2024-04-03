3ce40b036d57f65ada4d4790460f01608db68938
Nathan Walker <nathanwalker1208@gmail.com>
2024-04-03 15:29:04 -0600

    Add code to parse and serialize profile pic field

--- togetherly/lib/services/person_service.dart
+++ togetherly/lib/services/person_service.dart
@@ -61,7 +61,7 @@ class PersonService {
         id: map['id'],
         familyId: map['family_id'],
         name: map['name'],
-        icon: map['profile_pic'],
+        icon: _parseProfilePic(map['profile_pic']),
         totalPoints: map['total_points'],
       );
 
@@ -70,15 +70,15 @@ class PersonService {
         'family_id': child.familyId,
         'name': child.name,
         'is_parent': false,
+        'profile_pic': _profilePicToString(child.icon),
         'total_points': child.totalPoints,
-        'profile_pic': child.icon,
       };
 
   Parent _mapToParent(Map<String, dynamic> map) => Parent(
         id: map['id'],
         familyId: map['family_id'],
         name: map['name'],
-        icon: map['profile_pic'],
+        icon: _parseProfilePic(map['profile_pic']),
       );
 
   Map<String, dynamic> _parentToMap(Parent parent) => {
@@ -86,6 +86,38 @@ class PersonService {
         'family_id': parent.familyId,
         'name': parent.name,
         'is_parent': true,
-        'profile_pic': parent.icon,
+        'profile_pic': _profilePicToString(parent.icon),
       };
+
+  ProfileIcon _parseProfilePic(String pic) => switch (pic) {
+    "bear" => ProfileIcon.bear,
+    "cat" => ProfileIcon.cat,
+    "chicken" => ProfileIcon.chicken,
+    "dog" => ProfileIcon.dog,
+    "fish" => ProfileIcon.fish,
+    "fox" => ProfileIcon.fox,
+    "giraffe" => ProfileIcon.giraffe,
+    "gorilla" => ProfileIcon.gorilla,
+    "koala" => ProfileIcon.koala,
+    "panda" => ProfileIcon.panda,
+    "rabbit" => ProfileIcon.rabbit,
+    "tiger" => ProfileIcon.tiger,
+    _ => throw FormatException(
+        'Unsupported profile pic from database "$pic"'),
+  };
+
+  String _profilePicToString(ProfileIcon pic) => switch (pic) {
+    ProfileIcon.bear => "bear",
+    ProfileIcon.cat => "cat",
+    ProfileIcon.chicken => "chicken",
+    ProfileIcon.dog => "dog",
+    ProfileIcon.fish => "fish",
+    ProfileIcon.fox => "fox",
+    ProfileIcon.giraffe => "giraffe",
+    ProfileIcon.gorilla => "gorilla",
+    ProfileIcon.koala => "koala",
+    ProfileIcon.panda => "panda",
+    ProfileIcon.rabbit => "rabbit",
+    ProfileIcon.tiger => "tiger",
+  };
 }
