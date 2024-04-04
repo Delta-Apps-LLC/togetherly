584d713544e8a74c31cee7ebc202619e323d45a5
Nathan Walker <nathanwalker1208@gmail.com>
2024-04-04 13:01:38 -0600

    Modify ChoreService to accept SupabaseClient instance

--- togetherly/lib/services/chore_service.dart
+++ togetherly/lib/services/chore_service.dart
@@ -4,8 +4,12 @@ import 'package:togetherly/models/chore.dart';
 class ChoreService {
   static const String _choreTable = "chore";
 
+  const ChoreService(this.supabaseClient);
+
+  final SupabaseClient supabaseClient;
+
   Future<List<Chore>> getChoresByFamily(int familyId) async {
-    var result = await Supabase.instance.client
+    var result = await supabaseClient
         .from(_choreTable)
         .select("id, title, description, points, shared, date_due")
         .eq("family_id", familyId);
@@ -13,8 +17,7 @@ class ChoreService {
   }
 
   Future<Chore> insertChore(int familyId, Chore chore) async {
-    //Service function call and pass chore
-    await Supabase.instance.client
+    await supabaseClient
         .from(_choreTable)
         .insert(_choreToMap(chore, familyId));
     // TODO: Return selected chore from database
@@ -22,16 +25,14 @@ class ChoreService {
   }
 
   Future<void> deleteChore(Chore chore) async {
-    //await
-    await Supabase.instance.client
+    await supabaseClient
         .from(_choreTable)
         .delete()
         .match({'id': chore.id});
   }
 
   Future<void> updateChore(Chore chore) async {
-    //Query by choreID
-    await Supabase.instance.client
+    await supabaseClient
         .from(_choreTable)
         .update(_choreToMap(chore))
         .match({'id': chore.id});
