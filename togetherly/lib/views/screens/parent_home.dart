c009af276be9b935768cdeb64c52bc83e00dddb7
Andrew <a.thibs98@gmail.com>
2024-04-06 00:20:25 -0600

    added pull down to refresh on parent home

--- togetherly/lib/views/screens/parent_home.dart
+++ togetherly/lib/views/screens/parent_home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:togetherly/providers/chore_provider.dart';
+import 'package:togetherly/providers/person_provider.dart';
 import 'package:togetherly/themes.dart';
 import 'package:togetherly/views/widgets/chore_list.dart';
 import 'package:togetherly/views/widgets/family_chores_dialog.dart';
@@ -27,75 +30,89 @@ class ParentHomePage extends StatelessWidget {
       );
     }
 
+    Future<void> refresh() async {
+      final personProvider =
+          Provider.of<PersonProvider>(context, listen: false);
+      final choreProvider = Provider.of<ChoreProvider>(context, listen: false);
+      await personProvider.refresh();
+      await choreProvider.refresh();
+    }
+
     return Padding(
       padding: AppWidgetStyles.appPadding,
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const FamilyList(title: 'Members'),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Invisible grouping to make sure the title is centered
-                const Row(
-                  children: [
-                    IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.add,
-                        size: 32,
-                        color: Colors.transparent,
+      child: RefreshIndicator(
+        color: AppColors.brandGreen,
+        onRefresh: () => refresh(),
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const FamilyList(title: 'Members'),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Invisible grouping to make sure the title is centered
+                  const Row(
+                    children: [
+                      IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.add,
+                          size: 32,
+                          color: Colors.transparent,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    IconButton(
-                      onPressed: null,
-                      icon: Icon(
-                        Icons.arrow_forward_rounded,
-                        size: 32,
-                        color: Colors.transparent,
+                      SizedBox(
+                        width: 8,
                       ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'All Chores',
-                  style: AppTextStyles.brandAccentLarge.copyWith(fontSize: 22),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () => buildChoreDialog(context),
-                      icon: const Icon(
-                        Icons.add,
-                        size: 32,
-                        color: AppColors.brandBlack,
+                      IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.arrow_forward_rounded,
+                          size: 32,
+                          color: Colors.transparent,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    IconButton(
-                      onPressed: () => openChoreOverview(context),
-                      icon: const Icon(
-                        Icons.arrow_forward_rounded,
-                        size: 32,
-                        color: AppColors.brandBlack,
+                    ],
+                  ),
+                  Text(
+                    'All Chores',
+                    style:
+                        AppTextStyles.brandAccentLarge.copyWith(fontSize: 22),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => buildChoreDialog(context),
+                        icon: const Icon(
+                          Icons.add,
+                          size: 32,
+                          color: AppColors.brandBlack,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const ChoreList(
-              title: 'Today',
-            ),
-            const SizedBox(height: 20.0),
-          ],
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      IconButton(
+                        onPressed: () => openChoreOverview(context),
+                        icon: const Icon(
+                          Icons.arrow_forward_rounded,
+                          size: 32,
+                          color: AppColors.brandBlack,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const ChoreList(
+                title: 'Today',
+              ),
+              const SizedBox(height: 20.0),
+            ],
+          ),
         ),
       ),
     );
