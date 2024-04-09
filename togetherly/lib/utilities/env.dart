ba9833f29c9fe594265da667d6286c952243b35b
Nathan Walker <nathanwalker1208@gmail.com>
2024-04-09 09:13:43 -0600

    Move Supabase URL into env file

--- togetherly/lib/utilities/env.dart
+++ togetherly/lib/utilities/env.dart
@@ -5,5 +5,8 @@ part 'env.g.dart';
 @Envied(useConstantCase: true)
 abstract class Env {
   @EnviedField(obfuscate: true)
+  static final String supabaseUrl = _Env.supabaseUrl;
+
+  @EnviedField(obfuscate: true)
   static final String supabaseAnonKey = _Env.supabaseAnonKey;
 }
