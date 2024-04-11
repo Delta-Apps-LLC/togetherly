afe059618adf4afcd48ea51dbddcfcbceaf13cbf
Nathan Walker <nathanwalker1208@gmail.com>
2024-04-11 14:13:50 -0600

    Fix insert methods in PersonService

--- togetherly/lib/services/person_service.dart
+++ togetherly/lib/services/person_service.dart
@@ -34,7 +34,7 @@ class PersonService {
     return _mapToParent(
       (await _supabaseClient
               .from(_personTable)
-              .insert({_parentToMap(parent)}).select())
+              .insert(_parentToMap(parent)).select())
           .single,
     );
   }
@@ -43,7 +43,7 @@ class PersonService {
     return _mapToChild(
       (await _supabaseClient
               .from(_personTable)
-              .insert({_childToMap(child)}).select())
+              .insert(_childToMap(child)).select())
           .single,
     );
   }
