51a438efeaaf8db314ebe4a2f82b3aea695d2d0a
Ryan Harper <harper61.r@gmail.com>
2024-04-11 10:50:52 -0600

    Added a directory with files containing PostgresSQL for Subapabase

---
+++ togetherly/lib/Subabase_SQL/Misc.sql
@@ -0,0 +1,11 @@
+INSERT INTO Chore
+    VALUES ('1', 'Dishes', 'do it', '1961-06-16', 2, false, 1);
+
+INSERT INTO Chore
+    VALUES ('3', 'CS 428', 'Add more people to the team because we are getting behind - Reasonable Man Month', '2024-03-16', 8, true, 1);
+
+INSERT INTO Chore
+    VALUES ('4', 'Planting', 'a bomb', '2024-02-01', 100, false, 1);
+
+INSERT INTO Chore
+    VALUES ('5', 'Avoiding Homework', 'stress is bad for health', '2024-03-03', 8, false, 1);
