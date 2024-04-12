8be7893c242ead472f7d30c160b7fa82fa1f289b
Nathan Walker <nathanwalker1208@gmail.com>
2024-04-11 21:32:10 -0600

    Fix naming in ChoreCompletionService

--- togetherly/lib/services/chore_completion_service.dart
+++ togetherly/lib/services/chore_completion_service.dart
@@ -5,40 +5,41 @@ class ChoreCompletionService {
   static const String _completionTable = "chore_completion";
   static const String _familyCompletionView = "family_chore_completion";
 
-  Future<List<ChoreCompletion>> getAssignmentsByFamily(int familyId) async {
+  Future<List<ChoreCompletion>> getChoreCompletionsByFamily(int familyId) async {
     var result = await Supabase.instance.client
         .from(_familyCompletionView)
         .select(
             'id, chore_id, person_id, date_submitted, due_date, is_approved')
         .eq("family_id", familyId);
-    return result.map(_mapToCompletion).toList();
+    return result.map(_mapToChoreCompletion).toList();
   }
 
-  Future<ChoreCompletion> insertCompletion(ChoreCompletion completion) async {
-    return _mapToCompletion(
+  Future<ChoreCompletion> insertChoreCompletion(ChoreCompletion completion) async {
+    return _mapToChoreCompletion(
       (await Supabase.instance.client
               .from(_completionTable)
-              .insert(_completionToMap(completion))
+              .insert(_choreCompletionToMap(completion))
               .select())
           .single,
     );
   }
 
-  Future<void> deleteCompletion(ChoreCompletion completion) async {
+  Future<void> deleteChoreCompletion(ChoreCompletion completion) async {
     await Supabase.instance.client
         .from(_completionTable)
         .delete()
         .match({'id': completion.id});
   }
 
-  Future<void> updateCompletion(ChoreCompletion completion) async {
+  Future<void> updateChoreCompletion(ChoreCompletion completion) async {
     await Supabase.instance.client
         .from(_completionTable)
-        .update(_completionToMap(completion))
+        .update(_choreCompletionToMap(completion))
         .match({'id': completion.id});
   }
 
-  ChoreCompletion _mapToCompletion(Map<String, dynamic> map) => ChoreCompletion(
+  ChoreCompletion _mapToChoreCompletion(Map<String, dynamic> map) =>
+      ChoreCompletion(
         id: map['id'],
         choreId: map['chore_id'],
         childId: map['person_id'],
@@ -47,7 +48,7 @@ class ChoreCompletionService {
         isApproved: map['is_approved'],
       );
 
-  Map<String, dynamic> _completionToMap(ChoreCompletion completion) => {
+  Map<String, dynamic> _choreCompletionToMap(ChoreCompletion completion) => {
         'id': completion.id,
         'chore_id': completion.choreId,
         'person_id': completion.childId,
